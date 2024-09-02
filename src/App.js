8f50620df1238d9259a1f5fefa34ff705b904758
rajatrjsharma <merajatsharma@outlook.com>
2024-09-02 18:45:50 +0530

    Removal for test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import Contacts from './components/contacts/Contacts';
-import AddContact from './components/contacts/AddContact';
-import EditContact from './components/contacts/EditContact';
-import Header from './components/layout/Header';
-import About from './components/pages/About';
-import NotFound from './components/pages/NotFound';
-import Test from './components/test/Test';
+import React, { Component } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Contacts from "./components/contacts/Contacts";
+import AddContact from "./components/contacts/AddContact";
+import EditContact from "./components/contacts/EditContact";
+import Header from "./components/layout/Header";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
 
-import { Provider } from './context';
+import { Provider } from "./context";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -26,7 +25,6 @@ class App extends Component {
                 <Route exact path="/contact/add" component={AddContact} />
                 <Route exact path="/contact/edit/:id" component={EditContact} />
                 <Route exact path="/about" component={About} />
-                <Route exact path="/test" component={Test} />
                 <Route component={NotFound} />
               </Switch>
             </div>
